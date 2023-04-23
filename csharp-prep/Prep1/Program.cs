a1adb1b841829d5de7c41952b31ec436c1fe4318
Parker Jackman <jac21021@byui.edu>
2023-04-22 22:08:32 -0600

    Did all the prep work in 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your fistname? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your Last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {firstname}");
+        Console.WriteLine($"\nYour name is {lastname}, {firstname} {lastname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Hello, what grade percentage did you get in your Class: ");
+        string letter = "";
+        int score = int.Parse(Console.ReadLine());
+        if (score > 90)
+        {
+            letter = "A";
+        }
+        else if (score > 80)
+        {
+            letter = "B";
+        }
+         else if (score > 70)
+        {
+            letter = "C";
+        }
+         else if (score > 60)
+        {
+            letter = "D";
+        }
+         else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You received a {letter}.");
+        if (score > 70)
+        {
+            Console.WriteLine("You passed Congrats!!");
+        }
+        else
+        {
+            Console.WriteLine("You failed, but next time you will pass.");
+        }
+
     }
 }

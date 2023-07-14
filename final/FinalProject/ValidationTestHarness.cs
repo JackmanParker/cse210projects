05cb4a8ea75ff8c7dd4acda5e627dccfe52f5aff
Parker Jackman <parker.jackman@gmail.com>
2023-07-14 11:24:32 -0600

    Fixed some typos and verbage in test harness.

--- final/FinalProject/ValidationTestHarness.cs
+++ final/FinalProject/ValidationTestHarness.cs
@@ -11,18 +11,18 @@ namespace FinalProject
        }
        public void RunValidationTests()
        {    
-            Console.WriteLine("Type string you would like to validate: ");
+            Console.WriteLine("Type Number or phrase you would like to validate: ");
             string userInput = Console.ReadLine();
             IntegerValidator testIntegerValidator = new IntegerValidator();
             StringValidator testStringValidator = new StringValidator(); 
             Console.WriteLine("Checking integer Validation");
             Console.WriteLine($"Safe: {testIntegerValidator.CheckIfSafe(userInput)}");
-            Console.WriteLine($"Interger: {testIntegerValidator.CheckIfInteger(userInput)}");
+            Console.WriteLine($"Integer: {testIntegerValidator.CheckIfInteger(userInput)}");
             Console.WriteLine($"InRange 1,10: {testIntegerValidator.CheckRange(userInput, 1, 10)}");
             Console.WriteLine("Now Checking String Validation");
             Console.WriteLine($"Safe: {testStringValidator.CheckIfSafe(userInput)}");
             Console.WriteLine($"Is \"yes\" or \"no\": {testStringValidator.YesOrNo(userInput)}");
-            Console.WriteLine($"Lenght is in range of 1, 10: {testStringValidator.LengthCheck(userInput, 1, 10)}");
+            Console.WriteLine($"Length is in range of 1, 10: {testStringValidator.LengthCheck(userInput, 1, 10)}");
        }
 
        public void TestFilters()
